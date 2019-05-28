2f475b73e095eca959f41924c2c473dad54ce945
Ricardo Braga <ricardo.brgana@edu.unifor.br>
2019-05-28 20:17:00 -0300

    first commit

---
+++ Assets/Scripts/ComidaScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComidaScript : MonoBehaviour
+{
+    //public bool visivel = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    void comer()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
